6dfee9077c8f3deee72c731902456bb2bcf7fb5f
Chase Brenchley <chase.brenchley@gmail.com>
2018-10-03 10:48:41 -0600

    Added UI elements

--- script.js
+++ script.js
@@ -51,3 +51,60 @@ var skipper = setInterval(function (){
 		}
 	}
 }, 1000);
+
+var markVideoMenuButton = setInterval(function(){
+	contextMenu = document.getElementsByClassName("ytp-popup ytp-contextmenu");
+	console.log("Trying to place the button");
+	if (contextMenu.length === 1){
+			console.log("The menu exists! Placing button");
+
+			ytplayer = document.getElementById("movie_player");
+
+			var startTime;
+
+			var outer = document.createElement('div');
+			outer.classList = "ytp-menuitem";
+			outer.id = "context-butt";
+			// outer.addEventListener('mouseenter', function(){
+			//      ytplayer = document.getElementById("movie_player");
+			//      inner2.innerText = parseInt(ytplayer.getCurrentTime());
+			// })
+			outer.addEventListener('click', function(){
+					ytplayer = document.getElementById("movie_player");
+					if (!startTime){
+							startTime = ytplayer.getCurrentTime();
+							// alert("Marking " + startTime + " time as start of non-content")
+							inner1.innerText = "Mark as end";
+					}
+					else{
+							endTime = ytplayer.getCurrentTime();
+							// alert("Marking " + endTime + " time as end of non-content")
+
+							// Can now send both times to the server
+							alert("Sending to server");
+
+							startTime = endTime = null;
+							inner1.innerText = "Mark as start of non-content";
+
+}                       inner2.innerText = "";
+			});
+
+			var inner1 = document.createElement('div');
+			inner1.classList = "ytp-menuitem-label";
+			inner1.innerText = "Mark as start of 'non-content'";
+
+			// <div class="ytp-menuitem-content"></div>
+			var inner2 = document.createElement('div');
+			inner2.classList = "ytp-menuitem-content";
+			setInterval(function(){inner2.innerText = parseInt(ytplayer.getCurrentTime())}, 500);
+
+
+			outer.appendChild(inner1);
+			outer.appendChild(inner2);
+
+			var context = document.getElementsByClassName("ytp-panel-menu")[1]
+			context.insertBefore(outer, context.childNodes[0]);
+
+			clearInterval(markVideoMenuButton);
+	}
+}, 500);
