aea64508ac3d38bb14783ecea9b2cc05c1ac17b3
Chase Brenchley <chase.brenchley@gmail.com>
2018-10-03 15:14:06 -0600

    added identifiaction

--- extension/background.js
+++ extension/background.js
@@ -13,24 +13,28 @@ function youtube_parser(url){
     return (match&&match[7].length==11)? match[7] : false;
 }
 
-function get_time(id){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://127.0.0.1:5000/api/v1/resources/times?id="+id, false);
-    xhr.send();
-    return JSON.parse(xhr.response)[0]['time']
-}
+// function get_time(id){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "http://127.0.0.1:5000/api/v1/resources/times?id="+id, false);
+//     xhr.send();
+//     return JSON.parse(xhr.response)[0]['time']
+// }
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details){
-        // alert("You were about to navigate " + details.url);
-        // If there's not a tag, redirect
-        if(!/\&t=/.test(details.url)){
-            videoId = youtube_parser(details.url);
-            time = get_time(videoId);
-            return {redirectUrl: details.url+"&t="+time};
-        }
-        // return {redirectUrl: false};
-    }, 
-    {urls: ["https://www.youtube.com/watch*"]},
-    ["blocking"]
-);
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(details){
+//         // alert("You were about to navigate " + details.url);
+//         // If there's not a tag, redirect
+//         if(!/\&t=/.test(details.url)){
+//             videoId = youtube_parser(details.url);
+//             time = get_time(videoId);
+//             return {redirectUrl: details.url+"&t="+time};
+//         }
+//         // return {redirectUrl: false};
+//     }, 
+//     {urls: ["https://www.youtube.com/watch*"]},
+//     ["blocking"]
+// );
+
+chrome.identity.getProfileUserInfo(function(userInfo) {
+    console.log(JSON.stringify(userInfo));
+  });
